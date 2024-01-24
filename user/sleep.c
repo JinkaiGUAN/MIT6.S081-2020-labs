54701796ca3bb518fedc4fd178341e5689865342
PeterGUAN <jinkaiguan@outlook.com>
2024-01-24 23:14:26 +0800

    Print more information for sleep file!

--- user/sleep.c
+++ user/sleep.c
@@ -1,5 +1,4 @@
 #include "kernel/types.h"
-#include "kernel/stat.h"
 #include "user/user.h"
 
 int 
@@ -8,11 +7,26 @@ main(int argc, char* argv[])
 	// first we need to check the argument, print error msg if it does not pass an argument.
 	int n;
 	if (argc != 2) {
-		fprintf(2, "Please enter a number!\n");
+		fprintf(2, "Please enter a number after sleep command!\n");
 		exit(1);
 	} else {
-		n = atoi(argv[1]);
-		sleep(n);
-		exit(0);
+		int tag = 1;
+		char* p = argv[1];
+		while (*p){
+			if (*p < '0' || *p > '9') {
+				tag = 0;
+				break;
+			}
+			p++;
+		}
+		if (tag) {
+			n = atoi(argv[1]);
+			sleep(n);
+			fprintf(1, "We slept for %d seconds!", n);
+			exit(0);
+		} else {
+			fprintf(2, "We got wrong number!");
+			exit(1);
+		}
 	}
 }
